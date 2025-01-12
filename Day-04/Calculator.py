01a86efdfba7511a8f1218763f60a4e3e26eb49a
ayyappam4u <91835585+ayyappam4u@users.noreply.github.com>
2025-01-12 18:49:19 -0500

    Create Calculator.py

---
+++ Day-04/Calculator.py
@@ -0,0 +1,15 @@
+num1 = 12
+num2 = 12
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+print(add, sub)
+
